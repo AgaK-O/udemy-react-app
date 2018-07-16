3fe66d5a282a254062e3b4d6ec82b489d953fd02
AgaK-O <hembrita@op.pl>
2018-07-16 16:13:04 +0200

    before tweaks

--- src/App.js
+++ src/App.js
@@ -44,7 +44,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -66,18 +67,28 @@ class App extends Component {
           })}
         </div>
       )
+
+      style.backgroundColor = 'red';
+    }
+
+    let classes = [];
+    if(this.state.persons.length <= 2){
+      classes.push('red');
+    }
+    if(this.state.persons.length <= 1){
+      classes.push('bold');
     }
 
     return (
-      <div className="App">
-      
-        <h1>Hello, world.</h1> 
-        <button 
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle persons
-        </button>
-        {persons}
-      </div>
+        <div className="App">
+          <h1>Hello, world.</h1> 
+          <p className={classes.join(' ')}>This is working!</p>
+          <button 
+            style={style}
+            onClick={this.togglePersonsHandler}>Toggle persons
+          </button>
+          {persons}
+        </div>
     );
   }
 }
